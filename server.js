fba0aeac72926e509189b84c2c2b0ec0f8090548
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-03-23 11:44:47 +0100

    route

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 require("dotenv").config()
 const app = express()
 app.use(express.json())
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
+var api = require("./routes");
+app.use("/api", api);
 let port = 8000
 app.listen(port,() => {
     console.log("listen to port :",port)
